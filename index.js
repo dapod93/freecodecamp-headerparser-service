be8f473c5a61b401e61484c1813f17bf04ff25dc
dapod93 <davidliois50@gmail.com>
2025-03-16 19:28:46 +0700

    update: index
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
 });
 
 var listener = app.listen(process.env.PORT || 3000, function () {
